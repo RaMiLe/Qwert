33a8c8cd0677a38fded4ca378d3a107afd3e281f
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 22:11:42 +0300

    Create createtable.php

---
+++ createtable.php
@@ -0,0 +1,23 @@
+<?
+try {
+    $conn = new PDO("sqlsrv:server = tcp:ram1.database.windows.net,1433; Database = www", "ram1", "Rosbank1997");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "CREATE TABLE registration_on(
+id INT NOT NULL IDENTITY(1,1),
+PRIMARY KEY(id),
+name VARCHAR(30),
+email VARCHAR(30),
+gender VARCHAR(10),
+age VARCHAR(2),
+country VARCHAR(10),
+birthday VARCHAR(10),
+date DATE)";
+$conn->query($sql);
+
+echo "<h3>Таблица создана.</h3>";
+}
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
+?>
